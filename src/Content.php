09ca18363d1ae81c95005dc748f5cb00c4e60866
Ildar Usmanov <wajox@mail.ru>
2016-12-27 15:19:18 +0300

    fix

--- src/Content.php
+++ src/Content.php
@@ -4,11 +4,4 @@ namespace wajox\y2modcms\modules\content;
 class Content extends \wajox\yii2base\modules\ModuleAbstract
 {
 	public $hasSessionController = false;
-
-    protected function initModule()
-    {
-        $this->controllerNamespace = 'wajox\y2modcms\\'.$this->id.'\controllers';
-        $this->layout = 'profile';
-        $this->getApp()->user->loginUrl = ['/'.$this->id.'/session'];
-    }
 }
